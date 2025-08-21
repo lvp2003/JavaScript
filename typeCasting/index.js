2e094364153a21788da0d61bfea0a9ba36baeeac
Lakshmi Vara prasad <lvyadav999@gmail.com>
2025-08-21 20:08:57 +0530

    explainded about the type casting

---
+++ typeCasting/index.js
@@ -0,0 +1,58 @@
+// type conversion
+// chnage the data type of the varibale to another
+
+// int -> string
+// string -> int
+// int -> boolean , etc
+
+let x = "pizza";
+let y = "pizza";
+let z = "pizza";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+console.log("----------------------------------------");
+
+x = "100";
+y = "100";
+z = "100";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+console.log("----------------------------------------");
+
+x = "";
+y = "";
+z = "";
+
+x = Number(x);
+y = String(y);
+z = Boolean(z);
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z);
+
+let a;
+let b;
+let c;
+
+a = Number(a);
+b = String(b);
+c = Boolean(z);
+
+console.log(a, typeof a);
+console.log(b, typeof b);
+console.log(c, typeof z);
