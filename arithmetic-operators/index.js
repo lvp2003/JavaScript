e5ee1a6691ba998ed8831dd5349ba33da7286e2b
Lakshmi Vara prasad <lvyadav999@gmail.com>
2025-08-21 19:40:19 +0530

    explainded about the operators

---
+++ arithmetic-operators/index.js
@@ -0,0 +1,10 @@
+// arithetic operators = operands (values , varibale , etc.)
+// opertors (+ - * /)
+// ex ,
+// 11 = x + 55;
+
+let result = 5 + 100 + (7 % 122) + 999 - 76;
+console.log(result);
+
+result = 6 / 2 ** 6;
+console.log(result);
