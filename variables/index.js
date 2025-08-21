1af7534fb4034502d3d527e15b67da89f8ba031e
Lakshmi Vara prasad <lvyadav999@gmail.com>
2025-08-21 19:33:43 +0530

    explainded about the varibale , 2nd commit

---
+++ variables/index.js
@@ -0,0 +1,35 @@
+// varibale = A container thar stores a value
+// behaves as if it were the value it contains.
+
+//  1. declarations let x =;
+//  2.assignmenet x = 100;
+
+alert("hello");
+
+// var
+var name = "Lakshmi";
+console.log(name);
+
+// let
+let age = 21;
+age = 22;
+console.log(age);
+
+// const
+const country = "India";
+console.log(country);
+
+// different types
+let num = 100; // Number
+let str = "JavaScript"; // String
+let isGood = true; // Boolean
+let nothing = null; // Null
+let notAssigned; // Undefined
+let id = Symbol("id"); // Symbol
+let big = 999999999999999n; // BigInt
+
+let arr = [1, 2, 3]; // Array
+let obj = { x: 1, y: 2 }; // Object
+function greet() {
+  return "Hello";
+} // Function
---
+++ variables/index2.js
@@ -0,0 +1,11 @@
+let fullname = "lvp";
+let age = 21;
+let isStudent = true;
+
+console.log(`Fullname :${fullname}`);
+console.log(`age :${age}`);
+console.log(`Is student:${isStudent}`);
+
+document.getElementById("p1").textContent = `Your full name :${fullname}`;
+document.getElementById("p2").textContent = age;
+document.getElementById("p3").textContent = isStudent;
