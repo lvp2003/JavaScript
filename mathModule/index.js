688571612bbb76d9922487f18a79fc0f3e79c59e
Lakshmi Vara prasad <lvyadav999@gmail.com>
2025-08-21 23:20:26 +0530

    done with math module

---
+++ mathModule/index.js
@@ -0,0 +1,26 @@
+// Math = built-in that provides a
+// Collection of properties and methods
+
+let x = 3.5;
+let y = 2;
+let z = 1;
+
+z = Math.round(x);
+z = Math.floor(x);
+z = Math.ceil(x);
+z = Math.trunc(x);
+z = Math.pow(y,2);
+z = Math.sqrt(81);
+z = Math.log(x);
+z = Math.abs(-x);
+z = Math.cos(x);
+z = Math.tan(x);
+z = Math.cosh(x);
+z = Math.sinh(x);
+z = Math.tanh(x);
+z = Math.sign(x);
+
+let max = Math.max(1,2,3,4);
+console.log(max);
+let min = Math.min(1,2,3,4);
+console.log(min);
