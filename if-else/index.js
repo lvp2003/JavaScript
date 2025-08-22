4971dd2c03db1e9133de994ca16aab1f8e0712ee
Lakshmi Vara prasad <lvyadav999@gmail.com>
2025-08-22 07:07:25 +0530

    done with tthe if-else

---
+++ if-else/index.js
@@ -0,0 +1,18 @@
+document.getElementById("button").onclick = function () {
+  let age = Number(document.getElementById("age").value);
+  let output = document.getElementById("output");
+
+  if (isNaN(age)) {
+    output.textContent = "Please enter a valid number!";
+  } else if (age >= 100) {
+    output.textContent = "You are too old to enter this site.";
+  } else if (age == 0) {
+    output.textContent = "You can't enter, you are just born.";
+  } else if (age >= 18) {
+    output.textContent = "You are old enough to enter this site.";
+  } else if (age < 0) {
+    output.textContent = "Your age can't be below 0.";
+  } else {
+    output.textContent = "You must be 18+ to enter this site.";
+  }
+};
