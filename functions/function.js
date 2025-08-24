10c27d7fdbbae4fd2dea45f9d0545f68c2de2f64
Lakshmi Vara prasad <lakshmivaraprasadmaddiboina@gmail.com>
2025-08-24 10:07:50 +0530

    function concept

---
+++ functions/function.js
@@ -0,0 +1,21 @@
+// function = A section of code . Declare code once , use it whenever you wnat.
+// call the function to extute that code
+function happyBirthay() {
+  console.log("happy birthday");
+}
+
+happyBirthay();
+
+function happyBirthay2(username, num) {
+  console.log(username, num);
+}
+
+happyBirthay2("javascript", 30);
+
+// return
+
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(23, 27));
